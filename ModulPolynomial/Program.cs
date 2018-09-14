602f7646adbb33705ca697e60ef631a4dc7b05c8
Nikita <39918253+Giro5@users.noreply.github.com>
2018-09-14 21:21:16 +0500

    Add files via upload

---
+++ ModulPolynomial/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModulPolynomial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первый многочлен: ");
+            Polynom c1 = new Polynom { s = Console.ReadLine() };
+            Console.WriteLine("Введите второй многочлен: ");
+            Polynom c2 = new Polynom { s = Console.ReadLine() };
+
+        }
+    }
+    class Polynom
+    {
+        public string s;
+        public static Polynom Sum (Polynom a, Polynom b)
+        {
+            return new Polynom { };
+        }
+    }
+}
